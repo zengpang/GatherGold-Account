2954657005eb61a2c48644e358083396821b772f
曾胖 <2516629927@qq.com>
2022-12-17 21:40:34 +0800

    实现TabPage组件与功能

--- src/shared/TabPageBar.tsx
+++ src/shared/TabPageBar.tsx
@@ -1,31 +1,71 @@
 import s from './TabPageBar.module.scss';
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
+
 //分页导航栏
 export const TabPageBar = defineComponent({
+    props: {
+        selected: {
+            type: String as PropType<string>,
+            required: false
+        },
+        onUpdateSelected: {
+            type: Function as PropType<(name: string) => void>,
+            required: false,
+        }
+    },
     setup: (props, context) => {
         return () => {
-            const tabs=context.slots.default?.();
-            if(!tabs) return ()=>null;
-            for(let i=0;i<tabs.length;i++)
-            {
-                if(tabs[i].type!==TabPage)
-                {
-                    
+            const tabs = context.slots.default?.();
+            if (!tabs) return () => null;
+            //检测包含的所有子组件是否都是TabPage
+            for (let i = 0; i < tabs.length; i++) {
+                if (tabs[i].type !== TabPage) {
+                    throw new Error('<TabPageBar>只能包含TabPage');
                 }
             }
-            return <div class={s.tabPageBar}>
-                分页导航栏
+            return <><div class={s.tabPageBar}>
+                {tabs.map(item =>
+                    //在点击事件中更新父组件
+                    <TabButton class={[s.tabButton,item.props?.name===props.selected?s.selected:'']} onClick={()=>context.emit('update:selected', item.props?.name)}>{item.props?.name}</TabButton>
+                )}
             </div>
+                <div class={s.tabPageContainer}>
+                {tabs.find(item => item.props?.name === props.selected)}
+                </div>
+            </>
         }
 
 
     }
 })
-export const TabPage = defineComponent({
+//分页按钮
+export const TabButton = defineComponent({
+    props: {
+        onClick: {
+            type: Function as PropType<(e: MouseEvent) => void>
+        }
+    },
     setup: (props, context) => {
         return () => (
-            <div>
-              {context.slots.default?.()}
+            <button onClick={(props.onClick)}>
+                {context.slots.default?.()}
+            </button>
+        )
+    }
+});
+//分页
+export const TabPage = defineComponent({
+    props: {
+        name: {
+            type: String as PropType<string>
+        }
+
+    },
+    setup: (props, context) => {
+        console.log(props.name);
+        return () => (
+            <div >
+                {context.slots.default?.()}
             </div>
         )
     }
--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,15 +1,31 @@
 import { defineComponent,ref } from 'vue'
 import { RouterView } from 'vue-router'
 import { BottomBar } from '../shared/BottomBar'
-import { TabPageBar } from '../shared/TabPageBar'
+import { TabPageBar,TabPage } from '../shared/TabPageBar'
 import s from './MainPage.module.scss'
 
 export const MainPage=defineComponent({
     setup:(props,context)=>{
-        const refKind = ref('home')
+        const refKind = ref('home');
+        const refPageKind=ref('本月');
         return ()=>(
             <div class={s.wrapper}>
-                 <TabPageBar></TabPageBar>
+                 <TabPageBar v-model:selected={refPageKind.value}>
+                     <TabPage name='本月'>
+                         本月分页
+                         
+                     </TabPage>
+                     <TabPage name='上个月'>
+                         上个月分页
+                     </TabPage>
+                     <TabPage name='今年'>
+                         今年分页
+                     </TabPage>
+                     <TabPage name='其他'>
+                         其他分页
+                     </TabPage>
+                 </TabPageBar>
+                 
                  <h1>主页</h1>
                  <BottomBar v-model:selectName={refKind.value}></BottomBar>
             </div>
