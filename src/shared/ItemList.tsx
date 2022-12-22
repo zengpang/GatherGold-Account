eaa0b9ca43ba6703a42b546b9668e10afd1cce50
曾胖 <2516629927@qq.com>
2022-12-22 21:34:16 +0800

    封装TagList和ItemList为一体

--- src/shared/ItemList.tsx
+++ src/shared/ItemList.tsx
@@ -1,23 +1,44 @@
 import s from './ItemList.module.scss';
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import { Item } from './Item';
-
+import { TagItem } from './TagItem';
 export const ItemList = defineComponent({
   props: {
     Items: {
       type: Array as PropType<Array<any>>,
       required: false
+    },
+    ItemType: {
+      type: String as PropType<'bill' | 'tag'>,
+      required: true
     }
   },
+
   setup(props, context) {
     return () => {
       const Items = props.Items;
-      return <div class={s.wrapper}>
-          {Items?.map((item,index) => {
-          return (<Item tagIcon='\u{1F471}' tagName='旅行' tagPrice={1234} tagTime={"2000-01-01 12:39"}></Item>)
-        })}
-      </div>
-
+      const ItemType = props.ItemType;
+      switch (ItemType) {
+        case "bill": {
+          return <div class={s.wrapper}>
+            {Items?.map((item, index) => {
+              return (<Item tagIcon='\u{1F471}' tagName='旅行' tagPrice={1234} tagTime={"2000-01-01 12:39"}></Item>)
+            })}
+            <div class={s.footer}>没有更多</div>
+          </div>
+        };
+        case "tag": {
+          return <div class={s.wrapper}>
+            {Items?.map((item, index) => {
+              return (<TagItem tagIcon={item.sign} tagNumber={index + 1} tagName={item.name} class={s.item}></TagItem>)
+            })}
+            <div class={s.footer}>没有更多</div>
+          </div>
+        };
+        case undefined:{
+          return <div class={s.wrapper}>请输入有效参数</div>
+        }
+      }
     }
   }
 })
