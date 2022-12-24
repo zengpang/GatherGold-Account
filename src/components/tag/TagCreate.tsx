7be6f26af2ce832c59bb33651cab2c651c46bd57
曾胖 <2516629927@qq.com>
2022-12-24 22:50:37 +0800

    更新TagForm

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,6 +1,7 @@
-import s from './TagCreate.module.scss';
+import s from './Tag.module.scss';
 import { defineComponent, PropType, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
+import { Rules, validate } from '../../shared/validate';
 export const TagCreate =defineComponent({
    props:{
      name:{
@@ -15,8 +16,16 @@ export const TagCreate =defineComponent({
       const errors=reactive<{[k in keyof typeof formData]?:string[]}>({})
       const onSubmit=(e:Event)=>{
         const rules:Rules<typeof formData>=[
-          
+          { key: 'name', type: 'required', message: '必填' },
+          { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+          { key: 'sign', type: 'required', message: '必填' },     
         ]
+        Object.assign(errors,{
+          name:undefined,
+          sign:undefined
+        })
+        Object.assign(errors,validate(formData,rules));
+        e.preventDefault();
       }
       return ()=>(
         <MainLayout iconShow={true} iconPath={'/items/create'}>
--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,9 +1,51 @@
 import { defineComponent, PropType, reactive, toRaw } from 'vue';
 import { Button } from '../../shared/Button';
+import { EmojiSelect } from '../../shared/EmojiSelect';
+import {Form, FormItem } from '../../shared/Form';
+import { Rules,validate } from '../../shared/validate';
+import s from './Tag.module.scss';
 export const TagForm=defineComponent({
+   props:{
+     name:{
+      type:String as PropType<string>
+     }
+   },
    setup:(props,context)=>{
+     const formData=reactive({
+      name: '',
+      sign: '',
+     });
+     const errors=reactive<{[k in keyof typeof formData]?:string[]}>({})
+     const onSubmit=(e:Event)=>{
+      const rules: Rules<typeof formData> = [
+         { key: 'name', type: 'required', message: '必填' },
+         { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+         { key: 'sign', type: 'required', message: '必填' },
+       ]
+       Object.assign(errors,{
+         name:undefined,
+         sign:undefined
+       })
+       Object.assign(errors,validate(formData,rules));
+       e.preventDefault();
+     }
      return ()=>(
-        <h1>TabForm</h1>
+        <Form onSubmit={onSubmit}>
+           <FormItem label='标签名'
+              type="text"
+              v-model={formData.name}
+              error={errors['name']?.[0]}/>
+            <FormItem label={'符号'+formData.sign}
+              type="emojiSelect"
+              v-model={formData.sign}
+              error={errors['sign']?.[0]}/>
+            <FormItem>
+                <p class={s.tips}></p>
+            </FormItem>
+            <FormItem>
+                <Button class={[s.button]}>确定</Button>
+            </FormItem>
+        </Form>
      )
    } 
 })
