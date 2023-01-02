e6f396ed61708b25de3e62b32bc027ae3768e9bc
曾胖 <2516629927@qq.com>
2023-01-02 21:39:30 +0800

    更新Mock.tsx

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -26,34 +26,47 @@ const createId = () => {
     id += 1;
     return id;
 }
-export const mockItemIndex:Mock=(config)=>{
-    const {kind,page}=config.params;
-    const per_page=25;
-    const count=26;
-    const createPaper=(page=1)=>({
+export const mockItemIndex: Mock = (config) => {
+    const { kind, page } = config.params;
+    const per_page = 25;
+    const count = 26;
+    const createPaper = (page = 1) => ({
         page,
         per_page,
         count
     });
-    const createTag=(attrs?:any)=>({
-      id:createId(),
-      name:faker.lorem.word(),
-      sign:faker.internet.emoji(),
-      kind: 'expenses',
-      ...attrs
+    const createTag = (attrs?: any) => ({
+        id: createId(),
+        name: faker.lorem.word(),
+        sign: faker.internet.emoji(),
+        kind: 'expenses',
+        ...attrs
     });
-    const createItem=(n=1,attrs?:any)=>Array.from({length:n}).map(()=>({
-        id:createId(),
-        user_id:createId(),
-        amount:Math.floor(Math.random()*10000),
-        tags_id:[createId()],
-        tags:[createTag()],
-        happen_at:faker.date.past().toISOString(),
-        kind:config.params.kind,
+    const createItem = (n = 1, attrs?: any) => Array.from({ length: n }).map(() => ({
+        id: createId(),
+        user_id: createId(),
+        amount: Math.floor(Math.random() * 10000),
+        tags_id: [createId()],
+        tags: [createTag()],
+        happen_at: faker.date.past().toISOString(),
+        kind: config.params.kind,
     }))
-    const createBody=(n = 1, attrs?: any)=>({
-        
+    const createBody = (n = 1, attrs?: any) => ({
+        resources: createItem(n),
+        pager: createPaper(page),
+        summary: {
+            income: 9900,
+            expenses: 9900,
+            balance: 0
+        }
     })
+    if (!page || page === 1) {
+        return [200, createBody(25)];
+    } else if (page === 2) {
+        return [200, createBody(1)];
+    }else{
+        return [200,{}];
+    }
 
 }
 export const mockTagIndex: Mock = (config) => {
@@ -80,12 +93,11 @@ export const mockTagIndex: Mock = (config) => {
     else if (kind === 'expenses' && page === 2) {
         return [200, createBody(1)];
     }
-    else if(kind==='income'&&(!page||page===1))
-    {
-        return [200,createBody(25)];
+    else if (kind === 'income' && (!page || page === 1)) {
+        return [200, createBody(25)];
     }
-    else{
-        return [200,createBody(1)];
+    else {
+        return [200, createBody(1)];
     }
 
 
