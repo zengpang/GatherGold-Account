3240f30711086a0df31c8c42fa1dca4856e2771d
曾胖 <2516629927@qq.com>
2023-01-04 23:41:34 +0800

    更新Charts.tsx

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,15 +1,16 @@
-import { defineComponent, PropType, ref,computed } from 'vue';
+import { defineComponent, PropType, ref,computed, onMounted, watch } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 import { LineChart } from './LineChart';
 import { PieChart } from './PieChart';
 import { Bars } from './Bars';
 import { Time } from '../../shared/time';
+import { http } from '../../shared/Http';
 
 const DAY=24*3600*1000;
 type Data1Item={happen_at:string;amount:number};
 type Data1=Data1Item[];
-type Data2Item={tag_id:number;Tag:Tag;amount:number};
+type Data2Item={tag_id:number;tag:Tag;amount:number};
 type Data2=Data2Item[];
 export const Charts = defineComponent({
   props: {
@@ -32,7 +33,7 @@ export const Charts = defineComponent({
     // }
   },
   setup: (props, context) => {
-    const category = ref('expenses');
+    const kind = ref('expenses');
     const data1=ref<Data1>([]);
     const betterData1=computed<[string,number][]>(()=>{
       if(!props.startDate||!props.endDate)
@@ -45,16 +46,58 @@ export const Charts = defineComponent({
         const time=new Time(props.startDate+'T00:00:00.000+0800').add(i,'day').getTimestamp();
         const item=data1.value[0];
         const amount=item&&new Date(item.happen_at).getTime()=== time?data1.value.shift()!.amount:0;
-        
+        return [new Date(time).toISOString(),amount];
       })
     })
+    const fetchData1=async()=>{
+      const response=await http.get<{groups:Data1;summary:number}>('/items/summary',{
+        happen_after:props.startDate,
+        happen_before:props.endDate,
+        kind:kind.value,
+        group_by:'happen_at',
+        _mock:'itemSummary'
+      })
+      data1.value=response.data.groups;
+    }
+    onMounted(fetchData1);
+    watch(()=>kind.value,fetchData1);
+
+    const data2=ref<Data2>([]);
+    const betterData2=computed<{name:string;value:number}[]>(()=>
+      data2.value.map((item)=>({
+        name:item.tag.name,
+        value:item.amount
+      }))
+    )
+
+    const betterData3=computed<{tag:Tag;amount:number;percent:number}[]>(()=>{
+      const total=data2.value.reduce((sum,item)=>sum+item.amount,0);
+      return data2.value.map((item)=>({
+        ...item,
+        percent:Math.round((item.amount/total)*100);
+      }))
+    })
+
+    const fetchData2=async()=>{
+      const response=await http.get<{ groups: Data2; summary: number}>('/items/summary',{
+        happen_after:props.startDate,
+        happen_before:props.endDate,
+        kind:kind.value,
+        group_by:'tag_id',
+        _mock:'itemSummary'
+      })
+      data2.value=response.data.groups;
+    }
+    onMounted(fetchData2);
+    watch(()=>kind.value,fetchData2);
+
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type="select" options={[
           { value: 'expenses', text: '支出' },
           { value: 'income', text: '收入' }
-        ]} v-model={category.value} class={s.chartSelect} />
-        <LineChart />
+        ]} v-model={kind.value} class={s.chartSelect} />
+        <LineChart  />
         <PieChart />
         <Bars />
       </div>
