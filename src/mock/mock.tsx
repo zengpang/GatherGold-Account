e22e8c9feec66800e9106779ba0021d03da6da9c
曾胖 <2516629927@qq.com>
2023-01-05 17:29:19 +0800

    更新mock.tsx

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -3,6 +3,59 @@ import { AxiosRequestConfig } from "axios";
 
 type Mock = (config: AxiosRequestConfig) => [number, any];
 faker.setLocale('zh_CN');
+export const mockItemSummary:Mock=(config)=>{
+  const {group_by,kind}=config.params;
+  if(group_by==='happen_at' && kind==='expenses')
+  {
+     return [
+      200,{
+        groups:[
+          { happen_at: '2022-07-18T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2022-07-22T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2022-07-29T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+     ]
+  }else if(group_by==='happen_at'&&kind==='income')
+  {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: '2022-07-08T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2022-07-12T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2022-07-19T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  } else if (group_by === 'tag_id' && kind === 'expenses') {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 100 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  }else{
+    return [
+      200,
+      {
+        groups:[
+          { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 400 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
+        ],
+        summary: 900
+      }
+    ]
+  }
+}
 export const mockItemIndexBalance: Mock = config => {
   return [200, {
     expenses: 9900,
