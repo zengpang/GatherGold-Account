31809db76eb9b0c3695bfcabf89f8f0b7fe2e068
曾胖 <2516629927@qq.com>
2023-01-08 10:41:37 +0800

    更新me.tsx

--- src/shared/me.tsx
+++ src/shared/me.tsx
@@ -1,15 +1,11 @@
 import { AxiosResponse } from "axios";
 import { http } from "./Http"
 
-export let mePromise: Promise<AxiosResponse<{
-  resource: {
-    id: number;
-  };
-}>> | undefined
+export let mePromise: Promise<AxiosResponse<Resource<User>>> | undefined
 
 export const refreshMe = () => {
   console.log("刷新refreshMe");
-  mePromise = http.get<{ resource: { id: number } }>('/me')
+  mePromise = http.get<Resource<User>>('/me')
   return mePromise
 }
 
