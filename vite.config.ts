5bc4905bec0e2e6b39c2b3e5f43eb21302e0b755
zengpang <2516629927@qq.com>
2023-01-13 00:33:03 +0800

    初次更新

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn:true,
+      mergeProps:true
+    })
+  ],
+  server:{
+    host: '0.0.0.0',
+    proxy: {
+      //转发api路径代理设置
+      '/api/v1': {
+        target: 'http://121.196.236.94:3000/',//当输入相对路径/api/v1实际指向路径
+      }
+    }
+  },
+  base:'./'
+})
